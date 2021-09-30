445ac643e91505a489a216d4a5e00a1e79591b95
gquinn22 <70963304+gquinn22@users.noreply.github.com>
2021-09-30 17:33:43 -0400

    asd

--- gasketWindows/flower.js
+++ gasketWindows/flower.js
@@ -3,20 +3,21 @@
  * Assignment: Assignment 1
  * Title: spiral.js
  * */
+var canvas;
 var gl;
+var vBuffer;
+var triColor;
+var vColorLoc;
 var vertices = [];
+//var modelView; // variable modelView inherited from HTML
 
 window.onload = function init()
 {
-    var canvas = document.getElementById( "gl-canvas" );
+    canvas = document.getElementById( "gl-canvas" );
     
     gl = WebGLUtils.setupWebGL( canvas );
     if ( !gl ) { alert( "WebGL isn't available" ); }
 
-    //
-    //  Calling spiral to set up points array
-    //
-    square();
     
    
     gl.viewport( 0, 0, canvas.width, canvas.height );
@@ -26,11 +27,14 @@ window.onload = function init()
     
     var program = initShaders( gl, "vertex-shader", "fragment-shader" );
     gl.useProgram( program );
+
+    triColor = vec4(1.0, 0.0, 0.0, 1.0);
+    square();
     
     // Load the data into the GPU
-    
-    var bufferId = gl.createBuffer();
-    gl.bindBuffer( gl.ARRAY_BUFFER, bufferId );
+
+    vBuffer = gl.createBuffer();
+    gl.bindBuffer( gl.ARRAY_BUFFER, vBuffer );
     gl.bufferData( gl.ARRAY_BUFFER, flatten(vertices), gl.STATIC_DRAW );
 
     // Associate out shader variables with our data buffer
@@ -38,6 +42,10 @@ window.onload = function init()
     var vPosition = gl.getAttribLocation( program, "vPosition" );
     gl.vertexAttribPointer( vPosition, 2, gl.FLOAT, false, 0, 0 );
     gl.enableVertexAttribArray( vPosition );
+
+    vColorLoc = gl.getUniformLocation(program, "vColor");
+    modelView = gl.getUniformLocation(program, "modelView");
+    console.log("We made it");
 
     render();
 };
@@ -57,5 +65,24 @@ function square() {
 
 function render() {
     gl.clear( gl.COLOR_BUFFER_BIT );
-    gl.drawArrays( gl.LINE_LOOP, 0, vertices.length );
+    gl.uniform4fv(vColorLoc, triColor);
+    var mvMatrix = [];
+
+    mvMatrix = translate(0.0, 0.5, 0.0);
+    mvMatrix = mult(mvMatrix, scalem(0.5, 2.0, 1.0));
+    mvMatrix = mult(mvMatrix, rotate(-45.0, 0.0, 0.0, 1.0));
+    gl.uniformMatrix4fv(modelView, false, flatten(mvMatrix));
+    gl.drawArrays( gl.LINE_LOOP, 0, 4);
+    mvMatrix = mult(mvMatrix, translate(0.0, 0.1, 0.0));
+
+    for(var i = 1; i < 12; i++) {
+        //mvMatrix = rotate(-90.0, 0.0, 0.0, 1.0);
+        mvMatrix = mult(mvMatrix, translate(0.0, -0.5, 0.0));
+        //mvMatrix = mult(mvMatrix, rotate(-90.0, 0.0, 0.0, 1.0));
+        //mvMatrix = mult(mvMatrix, scalem(.5, 2.0, 1.0));
+        mvMatrix = mult(mvMatrix, rotate(-30*i, 0.0, 0.0, 1.0));
+        gl.uniformMatrix4fv(modelView, false, flatten(mvMatrix));
+        gl.drawArrays( gl.LINE_LOOP, 0, 4);
+    }
+    window.requestAnimFrame(render);
 }
